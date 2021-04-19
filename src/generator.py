05d2aaf230d198c24002a35a54768e48b3dbe216
marcus neo <marcus.neo418@gmail.com>
2021-04-19 10:02:28 +0800

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -16,9 +16,40 @@ class Generator:
             total raw outputs (open, high, low, close, vol)
         The array size is determined by:
             total_window -
+            windows discarded by past_window_size considerations -
             windows discarded by indicators -
-            windows discarded by
+            windows discarded by zero-tail outputs
+            (if last few rows are zeros, they'll be discarded until a non-zero output is found.
+            This is to increase the number of zero outputs as they are far too common)
+    Inputs:
+        *args : Additional commands to remove primary rows from output.
+            Possible commands are "no_open", "no_close", "no_high", "no_low", "no_volume"
 
+        past_window_size : how far in the past that you would want the model to have, in order to make its prediction.
+
+        prediction_length : if the model gives a signal, how long in the future does that signal stand?
+            (example if model says buy and TP not hit after (prediction_length) intervals, then it's signal is wrong)
+
+        total_window : initial timeframe taken from each sample (prior to shaving).
+
+        successful_trade_percent : take-profit percentage (for both long and short trades).
+
+        total_samples : Number of random samples in the ticker list to sample from.
+
+        ticker_list_directory : file location of the list of tickers.
+
+        **kwargs : Additional commands to add indicators. All indicators can be found at 
+        https://tulipindicators.org/list. Currently only support indicator type.
+
+        Add kwargs in the form of:
+        YOUR_INDICATOR_NAME={
+            "primary_columns": ["high", "low", "close"],    ## primary columns required to compute the indicator
+            "output_columns": 2,                            ## number of outputs that the indicator will provide
+            "period_list": [2, 3, 5],                       ## add any additional period params in order here
+        },
+
+    Outputs:
+        The array of lists to be used for training. 
 
 
     """
@@ -45,7 +76,8 @@ class Generator:
                 "total_window needs to be greater than past_window_size + prediction_length"
             )
         if successful_trade_percent <= 0.0:
-            raise ValueError("successful_trade_percent cannot be 0 or negative")
+            raise ValueError(
+                "successful_trade_percent cannot be 0 or negative")
         if total_samples <= 0:
             raise ValueError("total_samples cannot be 0 or negative")
         self.past_window_size = past_window_size
@@ -58,12 +90,27 @@ class Generator:
         self.kwargs = kwargs.items()
 
     def _custom_arguments(self, dataframe):
+        """
+            Adds indicator columns (if any) and remove primary columns (if requested)
+
+            inputs:
+                Original dataframe
+
+            outputs:
+                Processed dataframe with additional indicator columns and potential primary columns removed.
+
+        """
         dataframe = dataframe.astype("float64")
-        open_list = dataframe["Open"].to_numpy()  # pylint: disable=unused-variable
-        high_list = dataframe["High"].to_numpy()  # pylint: disable=unused-variable
-        low_list = dataframe["Low"].to_numpy()  # pylint: disable=unused-variable
-        close_list = dataframe["Close"].to_numpy()  # pylint: disable=unused-variable
-        volume_list = dataframe["Volume"].to_numpy()  # pylint: disable=unused-variable
+        open_list = dataframe["Open"].to_numpy(
+        )  # pylint: disable=unused-variable
+        high_list = dataframe["High"].to_numpy(
+        )  # pylint: disable=unused-variable
+        low_list = dataframe["Low"].to_numpy(
+        )  # pylint: disable=unused-variable
+        close_list = dataframe["Close"].to_numpy(
+        )  # pylint: disable=unused-variable
+        volume_list = dataframe["Volume"].to_numpy(
+        )  # pylint: disable=unused-variable
         smallest_output = len(dataframe)
         extra_column_dict = {}
 
@@ -114,8 +161,10 @@ class Generator:
             else:
                 for i in range(value["output_columns"]):
                     if len(custom_output[i]) < len(dataframe):
-                        smallest_output = min(smallest_output, len(custom_output[i]))
-                    extra_column_dict[class_method.__name__ + str(i)] = custom_output[i]
+                        smallest_output = min(
+                            smallest_output, len(custom_output[i]))
+                    extra_column_dict[class_method.__name__ +
+                                      str(i)] = custom_output[i]
         difference = len(dataframe) - smallest_output
         dataframe = dataframe.iloc[difference:]
         with pd.option_context("mode.chained_assignment", None):
@@ -126,12 +175,24 @@ class Generator:
                     dataframe[key] = new_value
                 else:
                     dataframe[key] = value
-        return dataframe, smallest_output
+        return dataframe
 
     def ticker_list(self):
+        """
+            Reads the csv file from the class attribute ticker_list_directory, and returns a pandas dataframe
+        """
         return pd.read_csv(self.ticker_list_directory, header=None)
 
     def _normalize_dataframe(self, dataframe):
+        """
+            Normalizes all columns in the dataframe (normalised with respect to only data within the column)
+
+            Input:
+                Unnormalized dataframe
+
+            Output:
+                Normalized dataframe
+        """
         vals = dataframe
         min_max_scaler = preprocessing.MinMaxScaler()
         scaled_vals = min_max_scaler.fit_transform(vals)
@@ -141,7 +202,14 @@ class Generator:
         return dataframe
 
     def _choose_sample(self):
-
+        """
+            Choose valid sample provided by the function ticker_list.
+            Examples of invalid samples are:
+                1. Samples with error outputs by yfinance library
+                2. Samples with Null values
+                3. Samples with less data than pass_window_size + prediction_length
+            Upon finding invalid samples, the function is run again, this time finding another random ticker.
+        """
         sample = self.ticker_list().sample().values.flatten()[0]
         try:
             ticker = yf.Ticker(sample)
@@ -158,16 +226,18 @@ class Generator:
         else:
             choices = length - (self.past_window_size + self.prediction_length)
             randomIndex = random.randint(0, choices)
-            return sample, hist.iloc[randomIndex : randomIndex + 100]
+            return sample, hist.iloc[randomIndex: randomIndex + 100]
 
     def _classify(self, preprocessed_data):
+        """
+            Generate output classifiers for individual data.
+        """
         close = preprocessed_data["Close"].to_numpy()
         high = preprocessed_data["High"].to_numpy()
         low = preprocessed_data["Low"].to_numpy()
         output = [0] * len(close)
         for i in range(len(close)):
             if i >= self.prediction_length:
-                # base_price = close[i]
                 offset = 1
                 position = 0
                 while (
@@ -196,7 +266,7 @@ class Generator:
             else:
                 pos -= 1
 
-        shaved_output = output[self.prediction_length : pos]
+        shaved_output = output[self.prediction_length: pos]
         return (
             shaved_output,
             (
@@ -225,20 +295,14 @@ class Generator:
         df = pd.DataFrame()
         for _ in range(self.total_samples):
             _, preprocessed_data = self._choose_sample()
-            processed_data, _ = self._custom_arguments(preprocessed_data)
-
+            processed_data = self._custom_arguments(preprocessed_data)
             normalized_data = self._normalize_dataframe(processed_data)
             total_columns = len(normalized_data.columns)
             (output_data, shaved_rows) = self._classify(preprocessed_data)
-            input_data = self._initial_parameters(normalized_data, total_columns)
-            input_data = input_data[self.past_window_size :]
+            input_data = self._initial_parameters(
+                normalized_data, total_columns)
+            input_data = input_data[self.past_window_size:]
             input_data = input_data[: (len(input_data) - shaved_rows - 1)]
-            # if len(input_data) > len(output_data):
-            #     input_data = input_data[: len(output_data)]
-            # print("preprocessed_data: ", len(preprocessed_data))
-            # print("output_data: ", len(output_data))
-            # print("input_data: ", len(input_data))
-            # print("shaved output: ", shaved_rows)
             if len(input_data) < len(output_data):
                 difference = len(output_data) - len(input_data)
                 output_data = output_data[difference:]
@@ -266,5 +330,4 @@ if __name__ == "__main__":
     )
 
     output_data_frame = trainingSet.generate()
-    # print(output_data_frame)
     output_data_frame.to_csv("sampleTrain.csv", index=False, header=False)
