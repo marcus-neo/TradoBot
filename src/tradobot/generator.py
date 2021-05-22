9b3e7e7e6d225e42b884df567b72fd652d75168d
marcus418 <56596363+marcus418@users.noreply.github.com>
2021-05-22 17:21:54 +0800

    Split the two generators

--- src/tradobot/generator.py
+++ src/tradobot/generator.py
@@ -5,32 +5,180 @@ import time
 import multiprocessing as mp
 import pandas as pd
 import yfinance as yf
-import numpy as np
-import tulipy as ti
-from sklearn import preprocessing
 
+from tradobot.utils.generator_utils import (
+    normalize_dataframe, 
+    custom_arguments,
+    classify,
+    initial_parameters,
+    max_indicator_length
+)
 
-def _normalize_dataframe(dataframe):
-    """Normalize all columns in the dataframe.
+class GenerateTest:
 
-    (normalised with respect to only data within the column)
+    """Create an array of lists.
 
-    Input:
-        Unnormalized dataframe
+        The list size is determined by:
+            total indicator outputs (rsi, etc) +
+            total raw outputs (open, high, low, close, vol)
+        The array size is determined by:
+            total_window -
+            windows discarded by past_window_size considerations -
+            windows discarded by indicators -
+            windows discarded by zero-tail outputs
+            (if last few rows are zeros, they'll be discarded until
+            a non-zero output is found. This is to increase the number
+            of zero outputs as they are far too common)
+    Inputs:
+        *args : Additional commands to remove primary rows from output.
+            Possible commands are
+                "no_open", "no_close", "no_high", "no_low", "no_volume"
 
-    Output:
-        Normalized dataframe
+        ordered_or_shuffled : Whether the data is ordered or randomly shuffled
+        past_window_size : how far in the past that you would want the model
+            to consider, in order to make its prediction.
+
+        prediction_length : if the model gives a signal,
+            how long in the future does that signal stand?
+            (example if model says buy and TP not hit after
+            (prediction_length) intervals, then it's signal is wrong)
+
+        random_dates_total_window : The window that will be used for
+            each sample, ending with the last available trading date.
+
+        fixed_dates_start : The start date
+
+        fixed_dates_end : The end date
+
+        successful_trade_percent : take-profit percentage
+            (for both long and short trades).
+
+        total_samples : Number of random samples in the
+            ticker list to sample from.
+
+        ticker_list_directory : file location of the list of tickers.
+
+        **kwargs : Additional commands to add indicators.
+            All indicators can be found at
+            https://tulipindicators.org/list.
+            Currently only support indicator type.
+
+        Add kwargs in the form of:
+        YOUR_INDICATOR_NAME={
+            # primary columns required to compute the indicator
+            "primary_columns": ["high", "low", "close"],
+            # number of outputs that the indicator will provide
+            "output_columns": 2,
+            # add any additional period params in order here
+            "period_list": [2, 3, 5],
+        },
+
+    Outputs:
+        The array of lists to be used for training.
     """
-    vals = dataframe
-    min_max_scaler = preprocessing.MinMaxScaler()
-    scaled_vals = min_max_scaler.fit_transform(vals)
-    dataframe = pd.DataFrame(
-        scaled_vals, columns=dataframe.columns, index=dataframe.index
-    )
-    return dataframe
+
+    def __init__(
+        self,
+        *args,
+        past_window_size=5,
+        prediction_length=5,
+        successful_trade_percent=5.0,
+        total_samples=200,
+        ticker_name="AAPL",
+        total_window=100,
+        fixed_dates_start=None,
+        fixed_dates_end=None,
+        **kwargs
+    ):
+        """Initialize the Generator class."""
+        # Error Catching
+        if past_window_size < 5:
+            raise ValueError(
+                "past_window_size is currently < 5, but is required to be >= 5"
+            )
+        if prediction_length < 1:
+            raise ValueError("prediction_length cannot be 0 or negative.")
+        if successful_trade_percent <= 0.0:
+            raise ValueError(
+                "successful_trade_percent cannot be 0 or negative"
+            )
+        if total_samples <= 0:
+            raise ValueError("total_samples cannot be 0 or negative")
+        self.past_window_size = past_window_size
+        self.prediction_length = prediction_length
+        self.fixed_dates_start = fixed_dates_start
+        self.fixed_dates_end = fixed_dates_end
+        self.successful_trade_percent = 0.01 * successful_trade_percent
+        self.ticker_name = ticker_name
+        self.total_samples = total_samples
+        self.total_window = total_window
+        self.args = args
+        self.kwargs = kwargs.items()
+        if fixed_dates_end is None and fixed_dates_start is None:
+            raise ValueError(
+                "either random_dates_total_window or"
+                "fix_dates input has to have a value"
+            )
+        if fixed_dates_end is None:
+            raise ValueError("fixed_dates_end needs to be filled in")
+        if fixed_dates_start is None:
+            raise ValueError("fixed_dates_start needs to be filled in")
+
+        end_datetime = datetime.strptime(fixed_dates_end, "%Y-%m-%d")
+        start_datetime = datetime.strptime(fixed_dates_start, "%Y-%m-%d")
+        if (
+            int((end_datetime - start_datetime).days / 7 * 5)
+            < max_indicator_length(self.kwargs)
+            + past_window_size
+            + prediction_length
+            + 1
+        ):
+            raise ValueError(
+                "too small time difference between startdate and enddate."
+            )
+
+    def _choose_sample(self):
+        start_date = date.today() - timedelta(
+            days=int(self.total_window / 5 * 7)
+        )
+        hist = yf.download(
+            self.ticker_name, start=start_date, end=date.today()
+        )
+        if len(hist.index) == 0:
+            raise RuntimeError("Failed Download, empty dataset")
+        if hist.isnull().values.any():
+            raise RuntimeError("Failed Download, NaN values identified.")
+        hist = hist.drop(columns=["Adj Close"])
+        return (self.ticker_name, hist)
+
+    def _create_df(self, preprocessed_data, dataframe):
+        processed_data = custom_arguments(
+            preprocessed_data, self.args, self.kwargs
+        )
+        normalized_data = normalize_dataframe(processed_data)
+        total_columns = len(normalized_data.columns)
+        input_data = initial_parameters(
+            normalized_data, total_columns, self.past_window_size
+        )
+        input_data = input_data[self.past_window_size :]  # noqa: E203
+        print(input_data[-1])
+        dataframe = pd.concat([dataframe, pd.DataFrame(input_data)]).tail(1)
+        dataframe = dataframe.sort_index()
+        return dataframe
+
+    def generate(self):
+        """Generate the test dataset."""
+        dataframe = pd.DataFrame()
+        ticker, preprocessed_data = self._choose_sample()
+        try:
+            dataframe = self._create_df(preprocessed_data, dataframe)
+        except ValueError as e:
+            print(str(e))
+            print("ticker: ", ticker)
+        return dataframe.fillna(0)
 
 
-class Generator:
+class GenerateTrain:
 
     """Create an array of lists.
 
@@ -102,9 +250,7 @@ class Generator:
         successful_trade_percent=5.0,
         total_samples=200,
         ticker_list_directory="../StockTickers/TickerNames.csv",
-        ticker_name="AAPL",
         random_dates_total_window=None,
-        total_window=100,
         fixed_dates_start=None,
         fixed_dates_end=None,
         **kwargs
@@ -136,9 +282,7 @@ class Generator:
         self.fixed_dates_end = fixed_dates_end
         self.successful_trade_percent = 0.01 * successful_trade_percent
         self.ticker_list_directory = ticker_list_directory
-        self.ticker_name = ticker_name
         self.total_samples = total_samples
-        self.total_window = total_window
         self.args = args
         self.kwargs = kwargs.items()
         if random_dates_total_window is None:
@@ -156,7 +300,7 @@ class Generator:
             start_datetime = datetime.strptime(fixed_dates_start, "%Y-%m-%d")
             if (
                 int((end_datetime - start_datetime).days / 7 * 5)
-                < self.max_indicator_length()
+                < max_indicator_length(self.kwargs)
                 + past_window_size
                 + prediction_length
                 + 1
@@ -168,7 +312,7 @@ class Generator:
         elif random_dates_total_window is not None:
             if (
                 random_dates_total_window
-                < self.max_indicator_length()
+                < max_indicator_length(self.kwargs)
                 + past_window_size
                 + prediction_length
                 + 1
@@ -178,241 +322,6 @@ class Generator:
                     "prediction_length + "
                     "the highest window size of the indicators"
                 )
-
-    # pylint: disable=too-many-branches, too-many-statements
-    def _custom_arguments(self, dataframe):
-        """Add indicator columns (if any).
-
-        and remove primary columns (if requested)..
-        inputs:
-            Original dataframe
-
-        outputs:
-            Processed dataframe with additional indicator columns and
-            potential primary columns removed.
-        """
-        dataframe = dataframe.astype("float64")
-        # pylint: disable=unused-variable
-        open_list = dataframe[  # noqa: F841
-            "Open"
-        ].to_numpy()  # pylint: disable=unused-variable
-        high_list = dataframe[  # noqa: F841
-            "High"
-        ].to_numpy()  # pylint: disable=unused-variable
-        low_list = dataframe[  # noqa: F841
-            "Low"
-        ].to_numpy()  # pylint: disable=unused-variable
-        close_list = dataframe[  # noqa: F841
-            "Close"
-        ].to_numpy()  # pylint: disable=unused-variable
-        volume_list = dataframe[  # noqa: F841
-            "Volume"
-        ].to_numpy()  # pylint: disable=unused-variable
-        smallest_output = len(dataframe)
-        extra_column_dict = {}
-        if (
-            all(
-                item in self.args
-                for item in [
-                    "no_open",
-                    "no_close",
-                    "no_high",
-                    "no_low",
-                    "no_volume",
-                ]
-            )
-        ) and not bool(self.kwargs):
-            raise ValueError(
-                "With all these input parameters, "
-                "there will be no nothing to evaluate on. \n"
-                "Either add indicators or do not remove all primary columns."
-            )
-        for item in self.args:
-            if item == "no_open":
-                dataframe = dataframe.drop(columns="Open")
-            elif item == "no_close":
-                dataframe = dataframe.drop(columns="Close")
-            elif item == "no_high":
-                dataframe = dataframe.drop(columns="High")
-            elif item == "no_low":
-                dataframe = dataframe.drop(columns="Low")
-            elif item == "no_volume":
-                dataframe = dataframe.drop(columns="Volume")
-            else:
-                raise ValueError("Invalid argument " + item)
-
-        for key, value in self.kwargs:
-            param = ""
-            if "open" in value["primary_columns"]:
-                param += "open_list"
-            if "high" in value["primary_columns"]:
-                if param:
-                    param += ", "
-                param += "high_list"
-            if "low" in value["primary_columns"]:
-                if param:
-                    param += ", "
-                param += "low_list"
-            if "close" in value["primary_columns"]:
-                if param:
-                    param += ", "
-                param += "close_list"
-            if "volume" in value["primary_columns"]:
-                if param:
-                    param += ", "
-                param += "volume_list"
-            if "period_list" in value:
-                for i in range(len(value["period_list"])):
-                    param += ", " + str(value["period_list"][i])
-            key = "".join(ch for ch in key if not ch.isupper())
-            class_method = getattr(ti, key)
-            # pylint: disable=eval-used
-            custom_output = eval("class_method" + "(" + param + ")")
-            if not isinstance(custom_output, tuple):
-                if len(custom_output) < len(dataframe):
-                    smallest_output = min(smallest_output, len(custom_output))
-                extra_column_dict[class_method.__name__] = custom_output
-            else:
-                for i in range(value["output_columns"]):
-                    if len(custom_output[i]) < len(dataframe):
-                        smallest_output = min(
-                            smallest_output, len(custom_output[i])
-                        )
-                    extra_column_dict[
-                        class_method.__name__ + str(i)
-                    ] = custom_output[i]
-        difference = len(dataframe) - smallest_output
-        dataframe = dataframe.iloc[difference:]
-        with pd.option_context("mode.chained_assignment", None):
-            for key, value in extra_column_dict.items():
-                new_difference = len(value) - smallest_output
-                if new_difference > 0:
-                    new_value = np.delete(value, slice(0, new_difference), 0)
-                    dataframe[key] = new_value
-                else:
-                    dataframe[key] = value
-        return dataframe
-
-    def _classify(self, preprocessed_data):
-        """Generate output classifiers for individual data."""
-        close = preprocessed_data["Close"].to_numpy()
-        high = preprocessed_data["High"].to_numpy()
-        low = preprocessed_data["Low"].to_numpy()
-        output = [0] * len(close)
-        for index, _ in enumerate(close):
-            if index >= self.prediction_length:
-                offset = 1
-                position = 0
-                while (
-                    index + offset < len(close) - 1
-                    and position == 0
-                    and offset <= self.prediction_length
-                ):
-                    offset += 1
-                    if (
-                        low[index + offset]
-                        <= (1 - self.successful_trade_percent) * close[index]
-                    ):
-                        position = -1
-                        break
-                    if (
-                        high[index + offset]
-                        >= (1 + self.successful_trade_percent) * close[index]
-                    ):
-                        position = 1
-                        break
-                output[index] = position
-        pos = len(output) - 1
-        for index, _ in enumerate(output):
-            if output[-index - 1] != 0:
-                break
-            pos -= 1
-
-        shaved_output = output[self.prediction_length : pos]  # noqa: E203
-        return (
-            shaved_output,
-            (
-                len(preprocessed_data.index)
-                - len(shaved_output)
-                - self.prediction_length
-            ),
-        )
-
-    def _initial_parameters(self, time_series, column_count):
-        """Create initial parameters."""
-        output_list = [([0] * (self.past_window_size * column_count))] * len(
-            time_series
-        )
-        for current_ts_position in range(len(time_series)):
-            if current_ts_position >= self.past_window_size:
-                initialize = [0] * (self.past_window_size * column_count)
-                for column_index in range(column_count):
-                    for t_minus_n in range(self.past_window_size):
-                        initialize[
-                            column_index * self.past_window_size + t_minus_n
-                        ] = time_series.iloc[
-                            current_ts_position - t_minus_n - 1
-                        ][
-                            column_index
-                        ]
-                output_list[current_ts_position] = initialize
-        return output_list
-
-    def max_indicator_length(self):
-        """Obtain the max indicator look-back from all indicators used."""
-        max_length = 0
-        for _, indicator_params in self.kwargs:
-            if "period_list" in indicator_params:
-                max_length = max(
-                    max_length, max(indicator_params["period_list"])
-                )
-        return max_length
-
-
-class GenerateTest(Generator):
-
-    """Class to generate the test dataset."""
-
-    def _choose_sample(self):
-        start_date = date.today() - timedelta(
-            days=int(self.total_window / 5 * 7)
-        )
-        hist = yf.download(
-            self.ticker_name, start=start_date, end=date.today()
-        )
-        if len(hist.index) == 0:
-            raise RuntimeError("Failed Download, empty dataset")
-        if hist.isnull().values.any():
-            raise RuntimeError("Failed Download, NaN values identified.")
-        hist = hist.drop(columns=["Adj Close"])
-        return (self.ticker_name, hist)
-
-    def _create_df(self, preprocessed_data, dataframe):
-        processed_data = self._custom_arguments(preprocessed_data)
-        normalized_data = _normalize_dataframe(processed_data)
-        total_columns = len(normalized_data.columns)
-        input_data = self._initial_parameters(normalized_data, total_columns)
-        input_data = input_data[self.past_window_size :]  # noqa: E203
-        print(input_data[-1])
-        dataframe = pd.concat([dataframe, pd.DataFrame(input_data)]).tail(1)
-        dataframe = dataframe.sort_index()
-        return dataframe
-
-    def generate(self):
-        """Generate the test dataset."""
-        dataframe = pd.DataFrame()
-        ticker, preprocessed_data = self._choose_sample()
-        try:
-            dataframe = self._create_df(preprocessed_data, dataframe)
-        except ValueError as e:
-            print(str(e))
-            print("ticker: ", ticker)
-        return dataframe.fillna(0)
-
-
-class GenerateTrain(Generator):
-
-    """Class to generate the training dataset."""
 
     def ticker_list(self):
         """Read the csv file from the class attribute ticker_list_directory.
@@ -482,12 +391,18 @@ class GenerateTrain(Generator):
             sample_ticker, preprocessed_data = self._choose_random_sample()
 
             try:
-                processed_data = self._custom_arguments(preprocessed_data)
-                normalized_data = _normalize_dataframe(processed_data)
+                processed_data = custom_arguments(
+                    preprocessed_data, self.args, self.kwargs
+                )
+                normalized_data = normalize_dataframe(processed_data)
                 total_columns = len(normalized_data.columns)
-                (output_data, shaved_rows) = self._classify(preprocessed_data)
-                input_data = self._initial_parameters(
-                    normalized_data, total_columns
+                (output_data, shaved_rows) = classify(
+                    preprocessed_data,
+                    self.prediction_length,
+                    self.successful_trade_percent,
+                )
+                input_data = initial_parameters(
+                    normalized_data, total_columns, self.past_window_size
                 )
                 input_data = input_data[self.past_window_size :]  # noqa: E203
                 input_data = input_data[: (len(input_data) - shaved_rows)]
