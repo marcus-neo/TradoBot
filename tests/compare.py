24e0f22eb0492a3bc4cf803941487ed677c759d2
marcus418 <56596363+marcus418@users.noreply.github.com>
2021-05-22 18:35:31 +0800

    add tolerance, changed yml file

--- tests/compare.py
+++ tests/compare.py
@@ -4,4 +4,4 @@ import numpy as np
 def compare_equal(true_output, test_output):
     test_array = np.array(test_output, dtype=np.float32)
     true_array = np.array(true_output, dtype=np.float32)
-    return np.allclose(true_array, test_array)
+    return np.allclose(true_array, test_array, atol=1e-03)
